25507933ad91b78484cb698c4bfc2356cee45071
Ubuntu <ubuntu@ip-172-31-2-203.ap-northeast-2.compute.internal>
2020-10-05 11:37:48 +0000

    2020.10.05

--- api/auth/certification.js
+++ api/auth/certification.js
@@ -2,7 +2,7 @@ const sms = require("../../util/sms");
 
 module.exports = async (req, res) => {
     let {us_phone_number} = req.body;
-    sms.set_us_phone_number(us_phone_number);
-    sms.send_sms()
+    // sms.set_us_phone_number(us_phone_number);
+    sms.send_sms();
     res.send("test");
 }
