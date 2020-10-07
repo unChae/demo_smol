9fbdd2f9e0c87fdaa864c92f504daac637eb260e
chae <darrun45@gmail.com>
2020-10-07 12:17:48 +0900

    2020.10.07[unchae] util.image modify

--- util/image.js
+++ util/image.js
@@ -13,7 +13,7 @@ module.exports = {
         return await new Promise((resolve, reject) => {
             request.get(uri, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
-                    data = "data:" + response.headers["content-type"] + ";base64," + Buffer.from(body).toString('base64');
+                    let data = "data:" + response.headers["content-type"] + ";base64," + Buffer.from(body).toString('base64');
                     resolve(data);
                 } else {
                     return reject(err);
@@ -22,13 +22,12 @@ module.exports = {
         })
     },
     s3_upload: (base64) => {
-        console.log(base64);
-        buf = Buffer.from(base64.replace(/^data:image\/\w+;base64,/, ""),'base64')
-        var param = {
+        let buffer = Buffer.from(base64.replace(/^data:image\/\w+;base64,/, ""),'base64')
+        let param = {
             'Bucket': process.env.AWS_S3_BUCKET,
             'Key': 'image/' + 'logo.png',
             'ACL': 'public-read',
-            'Body': buf,
+            'Body': buffer,
             'ContentEncoding': 'base64',
             'ContentType': 'image/jpeg'
         }
